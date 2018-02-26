1fa5a2ff7c9403a4b4a87a835545ac1acd325d24
BSLin <bslin@sfu.ca>
2018-02-25 22:28:41 -0800

    Created Activities, Created initial layout
    set activities to portrait only, created buttons and titles for each activity.

---
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/WalkingGroupsActivity.java
@@ -0,0 +1,13 @@
+package com.jade.walkinggroupbus.walkingschoolbus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WalkingGroupsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walking_groups);
+    }
+}
---
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/myGroupDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.jade.walkinggroupbus.walkingschoolbus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class myGroupDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_group_details);
+    }
+}
