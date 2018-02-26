7624bbf974b466cf92b84c4163554ccf9a506763
Junha Choi <junhac@sfu.ca>
2018-02-26 00:04:43 -0800

    Created scratch UI
    Created AddMonitoredUser, MoniotredUsers, MoniotredUserDetail Activities and UIs for them.

---
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/MoniterdUsers.java
@@ -0,0 +1,13 @@
+package com.jade.walkinggroupbus.walkingschoolbus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoniterdUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moniterd_users);
+    }
+}
