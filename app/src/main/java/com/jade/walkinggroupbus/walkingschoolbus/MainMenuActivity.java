17d41c8b48e25da22860a3ad05b51875dbcb4a9b
Richard Wong <rbw4@sfu.ca>
2018-02-27 02:34:36 -0800

    Basic UI Layout completed for login, registration, and main menu. Implemented basic registration into a Singleton Pattern.
    Was working on an incorrect project, so files would not push to Git.

---
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package com.jade.walkinggroupbus.walkingschoolbus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
