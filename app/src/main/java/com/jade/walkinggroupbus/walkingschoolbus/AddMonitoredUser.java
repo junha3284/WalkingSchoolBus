69079878a90a5012f076fe6deaefc198ef429ad2
Junha Choi <junhac@sfu.ca>
2018-03-06 18:24:20 -0800

    finished UI and partly implemented supports for that
    Finished general stream of Intents and scratch for backend implementations.
    Need to import server demo codes and merge with that.

--- app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/AddMonitoredUser.java
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/AddMonitoredUser.java
@@ -1,7 +1,12 @@
 package com.jade.walkinggroupbus.walkingschoolbus;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddMonitoredUser extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class AddMonitoredUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_monitored_user);
+        setAddBtn();
     }
+
+    void setAddBtn(){
+        Button btn_add = (Button) findViewById(R.id.button_add);
+        btn_add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                EditText edit_email = (EditText) findViewById(R.id.edit_email);
+                EditText edit_name = (EditText) findViewById(R.id.edit_name);
+                String string_email = edit_email.getText().toString();
+                String string_name = edit_name.getText().toString();
+                //TODO: use string_email and string_use to validate user and add this person to this user's MonitorsUsers.
+            }
+        });
+    }
+
+    public static Intent makeIntent(Context context){
+        Intent intent = new Intent(context,AddMonitoredUser.class);
+        return intent;
+    }
+
 }
---
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/Model/User.java
@@ -0,0 +1,107 @@
+package com.jade.walkinggroupbus.walkingschoolbus.Model;
+
+import com.jade.walkinggroupbus.walkingschoolbus.MoniterdUsers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by choijun-ha on 2018-03-06.
+ */
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    private List<User> monitoredByUsers = new ArrayList<>();
+    private List<User> monitorsUsers = new ArrayList<>();
+    private List<Void> walkingGroups = new ArrayList<>();   // <-- TO BE IMPLEMENTED
+
+    private String href;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<User> getMonitoredByUsers() {
+        return monitoredByUsers;
+    }
+
+    public void setMonitoredByUsers(List<User> monitoredByUsers) {
+        this.monitoredByUsers = monitoredByUsers;
+    }
+
+    public List<User> getMonitorsUsers() {
+        return monitorsUsers;
+    }
+
+    public void setMonitorsUsers(List<User> monitorsUsers) {
+        this.monitorsUsers = monitorsUsers;
+    }
+
+    public List<Void> getWalkingGroups() {
+        return walkingGroups;
+    }
+
+    public void setWalkingGroups(List<Void> walkingGroups) {
+        this.walkingGroups = walkingGroups;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", monitoredByUsers=" + monitoredByUsers +
+                ", monitorsUsers=" + monitorsUsers +
+                ", walkingGroups=" + walkingGroups +
+                '}';
+    }
+
+    @Override
+    public String toStringForList() {
+        return "name=" + name + '\'' +
+                ", email='" + email ;
+    }
+}
