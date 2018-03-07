527f84607b6b67b394e379f521beebe8563c1736
Richard Wong <rbw4@sfu.ca>
2018-03-07 13:10:07 -0800

    Code now utilizes a proxy server and accesses the server using Group Jade's API key.
    RegisterActivity now stores user credentials on the remote server.
    LoginActivity now queries the server for the inputted login data, and returns a token if valid.
    The majority of my code is now commented if clarification is needed.
    API Key is saved in strings.xml and can be accessed via the id: API_KEY

--- app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/model/UserInfo.java
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/model/UserInfo.java
@@ -1,16 +1,28 @@
 package com.jade.walkinggroupbus.walkingschoolbus.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInfo {
+    private Long id;
     private String email;
     private String password;
     private String name;
 
+    private List<UserInfo> monitoredByUsers = new ArrayList<>();
+    private List<UserInfo> monitorsUsers = new ArrayList<>();
+    private List<Void> walkingGroups = new ArrayList<>();   // <-- TO BE IMPLEMENTED
+
+    private String href;
+
+    // Singleton Implementation
     private static UserInfo instance;
 
     private UserInfo() {
         // private to prevent public instantiation
     }
 
+    // Checks to see if an instance of UserInfo already exists
     public static UserInfo userInfo(){
         if (instance == null){
             instance = new UserInfo();
@@ -40,5 +52,59 @@ public class UserInfo {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<UserInfo> getMonitoredByUsers() {
+        return monitoredByUsers;
+    }
+
+    public void setMonitoredByUsers(List<UserInfo> monitoredByUsers) {
+        this.monitoredByUsers = monitoredByUsers;
+    }
+
+    public List<UserInfo> getMonitorsUsers() {
+        return monitorsUsers;
+    }
+
+    public void setMonitorsUsers(List<UserInfo> monitorsUsers) {
+        this.monitorsUsers = monitorsUsers;
+    }
+
+    public List<Void> getWalkingGroups() {
+        return walkingGroups;
+    }
+
+    public void setWalkingGroups(List<Void> walkingGroups) {
+        this.walkingGroups = walkingGroups;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    // UI for Logcat messages.
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", monitoredByUsers=" + monitoredByUsers +
+                ", monitorsUsers=" + monitorsUsers +
+                ", walkingGroups=" + walkingGroups +
+                '}';
     }
 }
