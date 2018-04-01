f0db3b743b8cf778c9973e465810007fea2f2c60
Richard Wong <rbw4@sfu.ca>
2018-04-01 15:16:36 -0700

    Set up tabs for rewards centre

---
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/rewardscentrefragments/ShopFragment.java
@@ -0,0 +1,26 @@
+package com.jade.walkinggroupbus.walkingschoolbus.rewardscentrefragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jade.walkinggroupbus.walkingschoolbus.R;
+
+/**
+ * Created by Richard Wong on 2018-04-01.
+ */
+
+public class ShopFragment extends Fragment{
+    private static final String TAG = "MyRewardsFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_shop_tab, container, false);
+
+        return view;
+    }
+}
