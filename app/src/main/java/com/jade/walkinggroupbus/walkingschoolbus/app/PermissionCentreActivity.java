6f9ddb8b8ff9614c9a20d9ede72955957b842d8e
Junha Choi <junhac@sfu.ca>
2018-04-06 17:51:22 -0700

    Connected PermissionCentreActivity with PendingPermissionsActivity

--- app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/app/PermissionCentreActivity.java
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/app/PermissionCentreActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jade.walkinggroupbus.walkingschoolbus.R;
 
@@ -19,6 +21,14 @@ public class PermissionCentreActivity extends AppCompatActivity {
 
     void setBtns(){
         //TODO: connect permission-related activity with PermissionCentreActivity
+        Button btnPendingPermissions = (Button) findViewById (R.id.button_pendingPermissions);
+        btnPendingPermissions.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = PendingPermissionsActivity.makeIntent(PermissionCentreActivity.this);
+                startActivity(intent);
+            }
+        });
     }
 
     static Intent makeIntent(Context context){
