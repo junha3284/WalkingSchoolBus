414f5065c2bfa6abb667eb28323b7586fbce3a55
BSLin <bslin@sfu.ca>
2018-04-08 17:30:29 -0700

    set themes for new permission activities

--- app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/app/PreviousPermissionsActivity.java
+++ app/src/main/java/com/jade/walkinggroupbus/walkingschoolbus/app/PreviousPermissionsActivity.java
@@ -8,9 +8,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.jade.walkinggroupbus.walkingschoolbus.R;
+import com.jade.walkinggroupbus.walkingschoolbus.model.MyRewards;
 import com.jade.walkinggroupbus.walkingschoolbus.model.Permission;
 import com.jade.walkinggroupbus.walkingschoolbus.model.PermissionStatus;
 import com.jade.walkinggroupbus.walkingschoolbus.model.SharedData;
@@ -36,11 +38,25 @@ public class PreviousPermissionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // set theme
+        MyRewards myRewards = MyRewards.MyRewards();
+        setTheme(myRewards.getSelectedThemeID());
 
         //TODO: integrate theme change
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_previous_permissions);
+
+        // set background
+        if (!myRewards.getSelectedTheme().equals("Default")
+                && !myRewards.getSelectedTheme().equals("Dark")) {
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            img.setImageResource(myRewards.getSelectedImgID());
+        } else {
+            // if theme is default or dark, disable filter
+            View filter = (View) findViewById(R.id.filter);
+            filter.setVisibility(View.GONE);
+        }
 
         String token = sharedData.getToken();
         if(token != null)
